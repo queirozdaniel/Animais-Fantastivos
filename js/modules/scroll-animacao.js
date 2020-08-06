e1bb3afc1a0cfe295678dd29405cfd58382dcd18
queirozdaniel <danielqueirozak77@gmail.com>
2020-08-06 00:22:32 -0300

    Modificando Aminação do Scroll, adiciona debounce

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,22 +1,43 @@
+import debounce from "./debounce.js";
+
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 200);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop =
-        section.getBoundingClientRect().top - this.windowMetade;
-      if (sectionTop < 0) {
-        section.classList.add("ativo");
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener("scroll", this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  stop() {
+    /* remove animação no scroll */
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
